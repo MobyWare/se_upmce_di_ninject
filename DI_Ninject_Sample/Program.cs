0e074212cfe327123b8446ed674e5117e1087857
dickm <dickm@upmc.edu>
2016-05-18 08:26:14 -0400

    Initial commit from local

---
+++ DI_Ninject_Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Ninject.Modules;
+
+namespace DI_Ninject_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ DI_Ninject_Sample/upmce_emv_tests/test_mock_processors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace upmce_emv_tests
+{
+    [TestClass]
+    public class test_mock_processors
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
