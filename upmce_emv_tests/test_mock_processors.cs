89e31272c9e2892def7ec70538f825a488824d38
dickm <dickm@upmc.edu>
2016-05-23 15:07:39 -0400

    Added binding perf tests. Todo get late working

--- upmce_emv_tests/test_mock_processors.cs
+++ upmce_emv_tests/test_mock_processors.cs
@@ -3,36 +3,91 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject;
 using Ninject.Modules;
 using upmce_emv_common;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq;
 
 namespace upmce_emv_tests
 {
     [TestClass]
     public class test_mock_processors
     {
-
-        private IProcessor testProcessor;
+        private IKernel kernel;
 
         public test_mock_processors()
         {
-            var kernel = new StandardKernel();
-            kernel.Load(System.Reflection.Assembly.GetExecutingAssembly());
-            testProcessor = kernel.Get<IProcessor>();
-
+            kernel = new StandardKernel();
+            kernel.Load(System.Reflection.Assembly.GetExecutingAssembly());         
         }
         [TestMethod]
         public void ConfirmTestSetup()
         {
-            Assert.AreEqual(testProcessor.GetType().Name, typeof(TestAProcessor).Name);
+            Assert.AreEqual(kernel.Get<IProcessor>().GetType().Name, typeof(TestAProcessor).Name);
         }
 
         [TestMethod]
         public void ConfirmProcessReturnsValue()
         {
-            if(string.IsNullOrWhiteSpace(testProcessor.Process()))
+            if(string.IsNullOrWhiteSpace(kernel.Get<IProcessor>().Process()))
             {
                 Assert.Fail("Process call returned nothing");
             }
             
+        }
+
+        [TestMethod]
+        public void TimeNinjectBinding()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(kernel.Get<IProcessor>().Process()))
+                {
+                    Assert.Fail("Process call returned nothing");
+                }
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("There was an unexpected issue. Details: " + e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TimeLateBinding()
+        {
+            try
+            {
+                var dll = Assembly.LoadFile("upmce_emv_tests.dll");
+                Type localType = typeof(TestAProcessor);
+
+                localType = dll.GetTypes().Where(t => localType.IsAssignableFrom(t)).FirstOrDefault();
+
+                IProcessor localProcessor = (TestAProcessor)Activator.CreateInstance(localType);
+                if (string.IsNullOrWhiteSpace(localProcessor.Process()))
+                {
+                    Assert.Fail("Process call returned nothing");
+                }
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("There was an unexpected issue. Details: " + e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TimeEarlyBinding()
+        {
+            try
+            {
+                IProcessor localProcessor = new TestAProcessor();
+                if (string.IsNullOrWhiteSpace(localProcessor.Process()))
+                {
+                    Assert.Fail("Process call returned nothing");
+                }
+            }
+            catch(Exception e)
+            {
+                Assert.Fail("There was an unexpected issue. Details: " + e.Message);
+            }
         }
     }
 
